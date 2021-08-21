a3dddd2c0103b2707c9faa56b7a1aa2c0167093e
Ilya Aksenov <ilya@aksenov.ws>
2021-08-21 12:21:02 +0000

    and sort...

--- functions/sort.c
+++ functions/sort.c
@@ -5,6 +5,8 @@
 #include "stdlib.h"
 #include <time.h>
 
+#define TIME 330000
+#define SHIFT_ROW -10
                         //-------------------------- Функции
 int init_array (int A[], int n);
 void print_array (int A[], int n, int row, int col);
@@ -21,23 +23,23 @@ int main() {
                         //--------------- Инициализация цветовых пар
         start_color();                    // Запуск цветового режима
         init_pair(1, COLOR_CYAN, COLOR_BLACK);
-        init_pair(2, COLOR_GREEN, COLOR_RED);
+        init_pair(2, COLOR_GREEN, COLOR_BLACK);
         init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(4, COLOR_GREEN, COLOR_YELLOW);
+        init_pair(4, COLOR_RED, COLOR_BLACK);
         attron(COLOR_PAIR(1));          // Установка базовой пары
 
         int n;
         int arr [n];
-	mvprintw (10, 10, "Введите количество элементов в массиве = ");
+	mvprintw (LINES / 2 + SHIFT_ROW, 10, "Введите количество элементов в массиве = ");
 	scanw ("%d", &n);
        	init_array (arr, n); 
 	clear ();
-	print_array (arr, n, LINES / 2, (COLS - n * 4) / 2);
-	usleep (5000000);
+	print_array (arr, n, LINES / 2 + SHIFT_ROW, (COLS - n * 4) / 2);
+	usleep (TIME * 5);
 	sorting_choice (arr, n);
-	clear ();
-        print_array (arr, n, LINES / 2, (COLS - n * 4) / 2);
-
+	usleep (TIME *4);
+	attron (COLOR_PAIR(3));
+	mvprintw (LINES - 3, COLS - 45, "Для выхода нажмите любую клавишу >>> ");
         getch();
 
                         //--------------- Завершение режима ncurses
@@ -51,16 +53,16 @@ void sorting_choice(int A[], int n) {
         	int i_min = i;
         	int j;
 		int col_zero = (COLS - n * 4) / 2;
-		int row_zero = LINES / 2;
-        	for (j = i; j < n; j++) {
+		int row_zero = LINES / 2 + SHIFT_ROW;
+        	for (j = i + 1; j < n; j++) {
 			mvprintw (row_zero, col_zero + i_min * 4, "    ");
 			mvprintw (row_zero - 2, col_zero + i_min * 4, "%4d", A [i_min]);
 			refresh ();
-			usleep (500000);
+			usleep (TIME);
 			mvprintw (row_zero, col_zero + j * 4, "    ");
                         mvprintw (row_zero - 2, col_zero + j * 4, "%4d", A [j]);
                         refresh ();
-                        usleep (500000);
+                        usleep (TIME);
 			if (A [j] < A [i_min]) {
 	    			mvprintw (row_zero, col_zero + i_min * 4, "%4d", A [i_min]);
 				mvprintw (row_zero - 2, col_zero + i_min * 4, "    ");
@@ -73,6 +75,8 @@ void sorting_choice(int A[], int n) {
 			else {
 				mvprintw (row_zero - 2, col_zero + j * 4, "    ");
                         	mvprintw (row_zero, col_zero + j * 4, "%4d", A [j]);
+				refresh ();
+				usleep (TIME);
 			}
 	    
 	    	}	
@@ -80,27 +84,24 @@ void sorting_choice(int A[], int n) {
         	for (j = i_min; j > i; j--) {
            	
 			mvprintw (row_zero, col_zero + (j - 1) * 4, "    ");
-			usleep (500000);
+			usleep (TIME);
                         mvprintw (row_zero, col_zero + j  * 4, "%4d", A [j - 1]);
                         refresh ();
-                        usleep (500000);
+                        usleep (TIME);
 			A[j] = A[j - 1];
 
         	}
         	for (int k = i_min * 4; k > i * 4; k--)	{
 			mvprintw (row_zero - 2, col_zero + k, "%4d    ", tmp);
                         refresh ();
-			usleep (100000);
+			usleep (TIME / 4);
 		}
 		
-		mvprintw (row_zero - 2, col_zero + i * 4, "    ");
-		mvprintw (row_zero - 1, col_zero + i * 4, "%4d", tmp);
+		mvprintw (row_zero - 2, col_zero + i * 4, "      ");
 		refresh ();
-                usleep (250000);
-		mvprintw (row_zero - 1, col_zero + i * 4, "    ");
                 mvprintw (row_zero, col_zero + i * 4, "%4d", tmp);
                 refresh ();
-                usleep (250000);
+                usleep (TIME);
 		A[i] = tmp;
     	}
 }
@@ -118,7 +119,7 @@ void print_array (int A[], int n, int row, int col) {
         for (int i = 0; i < n; i++) {
                 mvprintw (row, col + i * 4, "%4d", A [i]);
                 refresh ();
-                usleep (90000);
+                usleep (TIME / 4);
         }
 }
 
