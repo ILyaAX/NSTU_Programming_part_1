70c50a8747dab8b7f4908451151ff2b1559a9e7f
Ilya Aksenov <ilya@aksenov.ws>
2021-08-22 14:00:10 +0000

    sort-sort

--- functions/sort.c
+++ functions/sort.c
@@ -1,13 +1,13 @@
 #include <ncurses.h>
-#include <string.h>
 #include <unistd.h>
 #include <locale.h>
 #include "stdlib.h"
 #include <time.h>
 
-#define TIME 330000
-#define SHIFT_ROW -7
-                        //-------------------------- Функции
+#define TIME 330000	// Отвечает за скорость воспроизведения
+#define SHIFT_ROW -6	// Смещение от центра печати массива
+                       
+			//-------------------------- Функции
 int init_array (int A[], int n);
 void print_array (int A[], int n, int row, int col);
 void sorting_choice(int A[], int n);
@@ -28,22 +28,30 @@ int main() {
         init_pair(4, COLOR_RED, COLOR_BLACK);
         attron(COLOR_PAIR(1));          // Установка базовой пары
 
-        int n;
-        int arr [n];
 	mvprintw (LINES / 2 + SHIFT_ROW, 10, "Введите количество элементов в массиве = ");
+	int n;	
 	scanw ("%d", &n);
-       	init_array (arr, n); 
 	clear ();
+	int arr [n];
+	init_array (arr, n); 
 	print_array (arr, n, LINES / 2 + SHIFT_ROW, (COLS - n * 4) / 2);
-	usleep (TIME * 5);
+
+			//---------------- Создание окна счетчиков
+        WINDOW *win_counters = newwin(7, 40, LINES / 2, (COLS - 38) / 2);
+        wattron(win_counters, COLOR_PAIR(1));
+        box (win_counters, 0, 0);
+        wrefresh (win_counters);
+
+	usleep (TIME * 2);
 	sorting_choice (arr, n);
 	attron (COLOR_PAIR(1));
 	mvprintw (LINES / 2 + SHIFT_ROW - 4, (COLS - 17) / 2, "Массив отсортирован");
 	refresh ();
 	usleep (TIME * 5);
+	attron (COLOR_PAIR(1) | A_BLINK);
 	mvprintw (LINES - 3, COLS - 45, "Для выхода нажмите любую клавишу >>> ");
         getch();
-
+	delwin(win_counters);
                         //--------------- Завершение режима ncurses
         endwin();
 
@@ -51,17 +59,21 @@ int main() {
 }
 
 void sorting_choice(int A[], int n) {
+	int comparisons = 0;
+	int shifts = 0;
 	int col_zero = (COLS - n * 4) / 2;
         int row_zero = LINES / 2 + SHIFT_ROW;
 	for (int i = 0; i < n - 1; i++) {
         	int i_min = i;
         	int j;
         	for (j = i + 1; j < n; j++) {
+			comparisons++;
 			attron (COLOR_PAIR(3));
 			mvprintw (row_zero, col_zero + i_min * 4, "    ");
 			mvprintw (row_zero - 2, col_zero + i_min * 4, "%4d", A [i_min]);
-			refresh ();
 			attron (COLOR_PAIR(1));
+			mvprintw (LINES / 2 + 2, (COLS - 30) / 2, "Количество сравнений = %d", comparisons);
+			refresh ();
 			usleep (TIME);
 			mvprintw (row_zero, col_zero + j * 4, "    ");
                         mvprintw (row_zero - 2, col_zero + j * 4, "%4d", A [j]);
@@ -88,7 +100,8 @@ void sorting_choice(int A[], int n) {
 	    	}	
         	int tmp = A[i_min];
         	for (j = i_min; j > i; j--) {
-           	
+           		shifts++;
+			mvprintw (LINES / 2 + 4, (COLS - 30) / 2, "Количество сдвигов = %d", shifts);	
 			mvprintw (row_zero, col_zero + (j - 1) * 4, "    ");
 			usleep (TIME);
                         mvprintw (row_zero, col_zero + j  * 4, "%4d", A [j - 1]);
@@ -130,7 +143,6 @@ void print_array (int A[], int n, int row, int col) {
         for (int i = 0; i < n; i++) {
                 mvprintw (row, col + i * 4, "%4d", A [i]);
                 refresh ();
-                usleep (TIME / 4);
+                usleep (TIME / 8);
         }
 }
-
